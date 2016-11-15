90a8ba30cc8a093f6eab0782397e05e1c213acc5
Robbie <txt3rob@gmail.com>
2016-11-15 14:20:28 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,4 +3,5 @@
 $db = new PDO("mysql:host=localhost;dbname=cracking", "cracking", "DATABASE_PASSWORD");
 $manager = "You@you.com";
 $serverPrefix = 'https://cracking.agartner.com/confirm.php?';
+$from = "noreply@you.com";
 ?>
